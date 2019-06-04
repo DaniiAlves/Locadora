c88de618dae70e246c865019e37688a506ee4c2a
DaniiAlves <nerfedwill@gmail.com>
2019-06-03 21:29:35 -0300

    Locadora

---
+++ interfaceGrafica/src/interfacegrafica/InterfaceGrafica.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacegrafica;
+
+/**
+ *
+ * @author aluno
+ */
+public class InterfaceGrafica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JanelaLogin jaaj = new JanelaLogin();
+        jaaj.setVisible(true);
+    }
+    
+}
